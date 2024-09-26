789c3025a2771aad6b9fac7a12b11e5008878a64
Joancgm05 <alu0101656571@ull.edu.es>
2024-09-26 16:14:22 +0100

    mc

---
+++ 3_Sum_of_three_integer_numbers/Sum_of_three_integer_numbers.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+  int number1, number2, number3;
+  std::cin >> number1 >> number2 >> number3;
+  std::cout << number1 + number2 + number3 << '\n';
+  return 0;
+}
+
